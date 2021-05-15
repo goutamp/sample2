2ec7296fbbf756ff4c8aded984ed8202803f9ee6
Goutam Pramanick <pramanick.goutam@gmail.com>
2021-05-15 20:32:45 +0530

    fix: sha fixing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Istioupdate_test_branch'
+    return 'Hello Istiosha_Fixing'
 
 @app.errorhandler(500)
 def server_error(e):
