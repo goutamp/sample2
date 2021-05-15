015d40fe07b442f151c12454152d2bb05d39a121
Goutam Pramanick <pramanick.goutam@gmail.com>
2021-05-15 19:05:53 +0530

    fix: this is fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Istioupdate'
+    return 'Hello Istioupdateone'
 
 @app.errorhandler(500)
 def server_error(e):
