95438761343991e1f25826964581173376e2e75f
Goutam Pramanick <pramanick.goutam@gmail.com>
2021-05-19 12:01:38 +0530

    fix: trivy update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello():
 
     """Return a friendly HTTP hello greeting."""
 
-    return 'Hello Istiosha_test2_fix_minor_patch_test_batch'
+    return 'Hello Istiosha_trivy_update'
 
 
 @app.errorhandler(500)
